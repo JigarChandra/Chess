643eb33d643b5a0824d41805704f8685acd80619
Jigar <jigar@freightwalla.com>
2018-12-10 11:08:02 +0530

    Test express

--- website/app.js
+++ website/app.js
@@ -65,17 +65,8 @@
 
 // }).listen(port);
 
-var static = require('node-static');
-const port = process.env.PORT || 3000
+var express = require('express'); 
+var app = express();
+app.use(express.static(__dirname));
 
-//
-var file = new static.Server('./website');
-
-require('http').createServer(function (request, response) {
-    request.addListener('end', function () {
-        //
-        // Serve files!
-        //
-        file.serve(request, response);
-    }).resume();
-}).listen(port);
+app.listen(process.env.PORT || 3000);
