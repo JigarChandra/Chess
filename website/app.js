77b764656d1b9cd1281200aa856e8ca4b9769681
Jigar <jigar@freightwalla.com>
2018-12-10 10:24:20 +0530

    Update app.js

--- website/app.js
+++ website/app.js
@@ -1,65 +1,66 @@
-const http = require('http');
+// const http = require('http');
+// const port = process.env.PORT || 3000
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/html');
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+
+//     // Request methods you wish to allow
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+//     // Request headers you wish to allow
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+//     // Set to true if you need the website to include cookies in the requests sent
+//     // to the API (e.g. in case you use sessions)
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+//     res.end('<h1>Hello World</h1>');
+// });
+// server.listen(port, () => {
+//     console.log(`Server running at port ` + port);
+// });
+
+
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
 const port = process.env.PORT || 3000
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.setHeader('Access-Control-Allow-Origin', '*');
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+http.createServer(function (request, response) {
+    console.log('request starting...');
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    var contentType = 'text/html';
 
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.end('<h1>Hello World</h1>');
-});
-server.listen(port, () => {
-    console.log(`Server running at port ` + port);
-});
+    fs.readFileSync(path.join(__dirname, './PlayerVsCPU.html'), function (error, content) {
+        if (error) {
+            if (error.code == 'ENOENT') {
+                fs.readFile('./404.html', function (error, content) {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                });
+            }
+            else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: ' + error.code + ' ..\n');
+                response.end();
+            }
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.setHeader('Access-Control-Allow-Origin', '*');
 
+            // Request methods you wish to allow
+            response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-// var http = require('http');
-// var fs = require('fs');
-// var path = require('path');
+            // Request headers you wish to allow
+            response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-// http.createServer(function (request, response) {
-//     console.log('request starting...');
+            // Set to true if you need the website to include cookies in the requests sent
+            // to the API (e.g. in case you use sessions)
+            response.setHeader('Access-Control-Allow-Credentials', true);
+            // response.end(content, 'utf-8');
+            res.end('<h1>Hello World</h1>');
+        }
+    });
 
-//     var contentType = 'text/html';
-
-//     fs.readFileSync(path.join(__dirname, './PlayerVsCPU.html'), function (error, content) {
-//         if (error) {
-//             if (error.code == 'ENOENT') {
-//                 fs.readFile('./404.html', function (error, content) {
-//                     response.writeHead(200, { 'Content-Type': contentType });
-//                     response.end(content, 'utf-8');
-//                 });
-//             }
-//             else {
-//                 response.writeHead(500);
-//                 response.end('Sorry, check with the site admin for error: ' + error.code + ' ..\n');
-//                 response.end();
-//             }
-//         }
-//         else {
-//             response.writeHead(200, { 'Content-Type': contentType });
-//             response.setHeader('Access-Control-Allow-Origin', '*');
-
-//             // Request methods you wish to allow
-//             response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-//             // Request headers you wish to allow
-//             response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-//             // Set to true if you need the website to include cookies in the requests sent
-//             // to the API (e.g. in case you use sessions)
-//             response.setHeader('Access-Control-Allow-Credentials', true);
-//             // response.end(content, 'utf-8');
-//             res.end('<h1>Hello World</h1>');
-//         }
-//     });
-
-// }).listen(3000);
+}).listen(port);
