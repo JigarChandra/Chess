4ca38f51c953564290bd53d672bcfdb498fec707
Jigar <jigar@freightwalla.com>
2018-12-10 11:14:32 +0530

    Update app.js

--- website/app.js
+++ website/app.js
@@ -67,6 +67,8 @@
 
 var express = require('express'); 
 var app = express();
+
+console.log('dir: ' + __dirname);
 app.use(express.static(__dirname + '/public'));
 
 app.listen(process.env.PORT || 3000);
