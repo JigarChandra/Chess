11327fc3d6137c16dc656216b882238e18ec7cf2
Jigar Chandra <jbc008@gmail.com>
2018-12-14 13:33:33 +0530

    Adds a new testing strategy for generating next attacking move

--- public/website/strategies/NextAttackingMoveGenerator.js
+++ public/website/strategies/NextAttackingMoveGenerator.js
@@ -1,44 +1,7 @@
 // TODO: find cumulative damage at an attacking position - for e.g trying to attack a bishop which is supported by a Queen, with two pawns
 // should result in a positive score attack 
 // For next best attacking move: Amplify capture by least valuable piece by giving higher weightage, search amongst only non-capture moves
-class AttackingMoveGenerator {
-	/*
-	Gets a move that results in a capture on the same turn with least possible damage amongst all other attacking moves
-	If 'atPos' is provided, then attempts to find best attacking move to capture piece at 'atPos'
-	*/
-	static getBestAttackingMove(gameInfo, atPos, fromPos) {
-			function isAttackingMove(move) {
-				if (typeof atPos === 'string') {
-					return (move.flags.includes("c") && move.to === atPos);
-				} else if (typeof fromPos === 'string') {
-					return (move.flags.includes("c") && move.from === fromPos);
-				}
-				return move.flags.includes("c");
-			}
-			var attackingMoves = gameInfo.moves({verbose:true}).filter(isAttackingMove);
-			if (attackingMoves.length > 0) {
-				var currBestScore = -100;
-				var currBestMove = null;
-				function getMoveWithBestScore(move) {
-					let piece1 = move.piece, piece2 = move.captured;
-					let piece1Score = PieceInfoGenerator.generateScore(piece1), piece2Score = PieceInfoGenerator.generateScore(piece2);
-					var damage = 0;
-					if (piece1Score >= piece2Score && ForeSightProvider.canGetCaptured(gameInfo, move.from, move.to)) {
-						damage = piece1Score;
-					}
-					let currScore = (piece2Score - damage);
-					if (currScore > currBestScore) {
-						currBestScore = currScore;
-						currBestMove = move.from + "-" + move.to;
-					}
-				}
-				attackingMoves.forEach(getMoveWithBestScore);
-				return {score: currBestScore, move: currBestMove};
-			} else {
-				return {score: -1, move: null}
-			}
-	}
-
+class NextAttackingMoveGenerator {
 	/*
 	TODO: Does not handle en-passant
 	Excellent e.g: 'rnbqkbnr/p1p1pppp/1p1p4/8/8/2N3P1/PPPPPP1P/R1BQKBNR w KQkq - 0 3', will result in white bishop from f1 to g2 and
@@ -88,15 +51,18 @@ class AttackingMoveGenerator {
 					var currCapturedByScore =  Number.MAX_SAFE_INTEGER;
 
 					if (ForeSightProvider.getBestCapture(gameInfo, piece1, move.from, move.to) > piece1Score &&
-						!ForeSightProvider.canGetCaptured(gameInfo, move.from, move.to)
+						//!ForeSightProvider.canGetCaptured(gameInfo, move.from, move.to)
+						AttackingMoveGenerator.getBestAttackingMove(gameInfo, move.to).score < 0
 						) {
 
 						let futGame = new Chess(gameInfo.fen());
 						futGame.move(move.from + '-' + move.to, {sloppy: true});
 						let opponentBestAttackingMove = AttackingMoveGenerator.getBestAttackingMove(futGame);
 
-						if (opponentBestAttackingMove.score <= currOpponentBestAttackingMove.score &&
-						 !ForeSightProvider.canGetCaptured(gameInfo, move.from, move.to)) {
+						// ensure that this move does not make any other piece vulnerable
+						if ((opponentBestAttackingMove.score <= currOpponentBestAttackingMove.score 
+						 && !ForeSightProvider.canGetCaptured(gameInfo, move.from, move.to)
+						 ) || (opponentBestAttackingMove.score < 0)) {
 							currScore = ForeSightProvider.getBestCapture(gameInfo, piece1, move.from, move.to);
 							currCapturedByScore = piece1Score;
 						}
--- public/website/strategies/NextOffensiveSubStrategy.js
+++ public/website/strategies/NextOffensiveSubStrategy.js
@@ -1,6 +1,6 @@
 class NextOffensiveSubStrategy {
 	move(gameInfo) {
-		var res = AttackingMoveGenerator.getNextBestAttackingMove(gameInfo);
+		var res = NextAttackingMoveGenerator.getNextBestAttackingMove(gameInfo);
 		res.strategy = 'NextOffensive'
 		return res;
 		// TODO: implement ii & iii
